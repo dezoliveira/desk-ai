0916003e7e9edb785e961255a7cedd9951049b3f
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-20 13:47:22 -0300

    Stats Add at dashboard

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import { database } from '@/firebaseConfig'
 import { get, ref } from 'firebase/database'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
@@ -36,8 +37,82 @@ export default function Home() {
   const max = score.total
 
   return (
-    <main>
-      <div>
+    <main class="wrapper sm:flex sm:justify-center">
+      <section class="flex flex-col gap-4">
+
+        <span class="stat-title">
+          <h2 class="text-2xl text-center">
+            Totais
+          </h2>
+        </span>
+
+        <div class="stats shadow">
+    
+          <div class="stat place-items-center">
+            <div class="stat-title">Totais</div>
+            <div class="stat-value text-primary">{score.total}</div>
+            <div class="stat-desc">Do ano de 2024</div>
+          </div>
+
+          <div class="stat place-items-center">
+            <div class="stat-title">Em aberto</div>
+            <div class="stat-value text-secondary">{score.open}</div>
+            <div class="stat-desc">Do ano de 2024</div>
+          </div>
+
+          <div class="stat place-items-center">
+            <div class="stat-title">Em andamento</div>
+            <div class="stat-value text-accent">{score.working}</div>
+            <div class="stat-desc">Do ano de 2024</div>
+          </div>        
+          
+        </div>
+
+        <span class="stat-title">
+          <h2 class="text-2xl text-center">
+            Prioridades
+          </h2>
+        </span>
+        
+        <div className='stats shadow'>
+
+          <div class="stat">
+            <div class="stat-figure text-success">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            </div>
+            <div class="stat-title">Alta</div>
+            <div class="stat-value text-success">31K</div>
+            <div class="stat-desc">Jan 1st - Feb 1st</div>
+          </div>
+          
+          <div class="stat">
+            <div class="stat-figure text-warning">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            </div>
+            <div class="stat-title">Média</div>
+            <div class="stat-value text-warning">31K</div>
+            <div class="stat-desc">Jan 1st - Feb 1st</div>
+          </div>
+          
+          <div class="stat">
+            <div class="stat-figure text-error">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            </div>
+            <div class="stat-title">Baixa</div>
+            <div class="stat-value text-error">31K</div>
+            <div class="stat-desc">Jan 1st - Feb 1st</div>
+          </div>
+        </div>
+
+        <div class="flex justify-center align-center py-10">
+          <Link href="/tickets">
+            <button className='btn btn-primary'>
+              Ver Chamados
+            </button>
+          </Link>
+        </div>
+      </section>
+      {/* <div>
         <h1>Totais:</h1>
         <h2>Total: {score.total}</h2>
         <h2>Em aberto: {score.open}</h2>
@@ -58,7 +133,7 @@ export default function Home() {
               <span className="visually-hidden">Loading...</span>
           </>
         }
-      </div>
+      </div> */}
     </main>
   );
 }
