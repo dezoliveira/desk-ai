d9967a21c8362b40507255776a4e5120956ef970
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-20 14:29:10 -0300

    Navbar styles and structure

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -10,22 +10,37 @@ const navColappse = {
 export default function MainNavbar() {
   return (
     <main>
-      <div>
-        <div>
-          <span>
-            <h4 className='m-0 text-white'>DESK AI</h4>
-          </span>
-          <span id="basic-navbar-nav">
-            <nav className='d-flex align-items-center gap-4'>
-              <Link href="/" className='link'>Home</Link>
-              <Link href="/tickets" className='link'>Chamados</Link>
-              <Link href="/tickets/create">
-                <button variant='danger'>
-                  Abrir Chamado
-                </button>
-              </Link>
-            </nav>
-          </span>
+      <div class="navbar bg-base-100">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl">daisyUI</a>
+          <nav class="flex items-center align-center gap-4">
+            <Link href="/" className='link'>Home</Link>
+            <Link href="/tickets" className='link'>Chamados</Link>
+            <Link href="/tickets/create">
+              <button class='btn btn-primary'>
+                Abrir Chamado
+              </button>
+            </Link>
+          </nav>
+        </div>
+        <div class="flex-none gap-2">
+          <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+              <div class="w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </div>
+            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+              <li>
+                <a class="justify-between">
+                  Profile
+                  <span class="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,6 @@ module.exports = {
     require('daisyui'),
   ],
   daisyui: {
-    themes: ["nord"],
+    themes: ["forest"],
   },
 }
